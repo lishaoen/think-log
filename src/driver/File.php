74f6453c3aaf5a69710a66c67115396280e5f437
lishaoenbh@qq.com <lishaoenbh@qq.com>
2018-10-09 16:28:25 +0800

    提交1.0.1版本

---
+++ src/driver/File.php
@@ -0,0 +1,238 @@
+<?php
+// +----------------------------------------------------------------------
+// | think_lishaoen [基于ThinkPHP_V5.1开发]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2018 lishaoen.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: lishaoen <lishaoenbh@qq.com>
+// +----------------------------------------------------------------------
+namespace lishaoen\log\driver;
+
+/**
+ * 本地化调试输出到文件
+ */
+class File
+{
+    protected $config = [
+        'time_format' => ' c ',
+        'single'      => false,
+        'file_size'   => 2097152,
+        'path'        => '',
+        'apart_level' => [],
+        'max_files'   => 0,
+        'json'        => false,
+    ];
+
+    protected $logPath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR;
+
+    // 实例化并传入参数
+    public function __construct($config = [])
+    {
+        if (is_array($config)) {
+            $this->config = array_merge($this->config, $config);
+        }
+        if(empty($this->logPath)){
+            $this->logPath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR;
+        }
+        
+        if (empty($this->config['path'])) {
+            $this->config['path'] = $this->logPath . 'log' . DIRECTORY_SEPARATOR;
+        } elseif (substr($this->config['path'], -1) != DIRECTORY_SEPARATOR) {
+            $this->config['path'] .= DIRECTORY_SEPARATOR;
+        }
+    }
+
+    /**
+     * 日志写入接口
+     * @access public
+     * @param  array    $log    日志信息
+     * @param  bool     $append 是否追加请求信息
+     * @return bool
+     */
+    public function save(array $log = [], $append = false)
+    {
+        $destination = $this->getMasterLogFile();
+
+        $path = dirname($destination);
+        !is_dir($path) && mkdir($path, 0755, true);
+
+        $info = [];
+
+        foreach ($log as $type => $val) {
+
+            foreach ($val as $msg) {
+                if (!is_string($msg)) {
+                    $msg = var_export($msg, true);
+                }
+
+                $info[$type][] = $this->config['json'] ? $msg : '[ ' . $type . ' ] ' . $msg;
+            }
+
+            if (!$this->config['json'] && (true === $this->config['apart_level'] || in_array($type, $this->config['apart_level']))) {
+                // 独立记录的日志级别
+                $filename = $this->getApartLevelFile($path, $type);
+
+                $this->write($info[$type], $filename, true, $append);
+
+                unset($info[$type]);
+            }
+        }
+
+        if ($info) {
+            return $this->write($info, $destination, false, $append);
+        }
+
+        return true;
+    }
+
+    /**
+     * 日志写入
+     * @access protected
+     * @param  array     $message 日志信息
+     * @param  string    $destination 日志文件
+     * @param  bool      $apart 是否独立文件写入
+     * @param  bool      $append 是否追加请求信息
+     * @return bool
+     */
+    protected function write($message, $destination, $apart = false, $append = false)
+    {
+        // 检测日志文件大小，超过配置大小则备份日志文件重新生成
+        $this->checkLogSize($destination);
+
+        // 日志信息封装
+        $info['timestamp'] = date($this->config['time_format']);
+
+        foreach ($message as $type => $msg) {
+            $info[$type] = is_array($msg) ? implode("\r\n", $msg) : $msg;
+        }
+
+        if (PHP_SAPI == 'cli') {
+            $message = $this->parseCliLog($info);
+        } else {
+            $message = $this->parseLog($info);
+        }
+
+        return error_log($message, 3, $destination);
+    }
+
+    /**
+     * 获取主日志文件名
+     * @access public
+     * @return string
+     */
+    protected function getMasterLogFile()
+    {
+        if ($this->config['max_files']) {
+            $files = glob($this->config['path'] . '*.log');
+
+            try {
+                if (count($files) > $this->config['max_files']) {
+                    unlink($files[0]);
+                }
+            } catch (\Exception $e) {
+            }
+        }
+
+        if ($this->config['single']) {
+            $name = is_string($this->config['single']) ? $this->config['single'] : 'single';
+
+            $destination = $this->config['path'] . $name . '.log';
+        } else {
+            $cli = PHP_SAPI == 'cli' ? '_cli' : '';
+
+            if ($this->config['max_files']) {
+                $filename = date('Ymd') . $cli . '.log';
+            } else {
+                $filename = date('Ym') . DIRECTORY_SEPARATOR . date('d') . $cli . '.log';
+            }
+
+            $destination = $this->config['path'] . $filename;
+        }
+
+        return $destination;
+    }
+
+    /**
+     * 获取独立日志文件名
+     * @access public
+     * @param  string $path 日志目录
+     * @param  string $type 日志类型
+     * @return string
+     */
+    protected function getApartLevelFile($path, $type)
+    {
+        $cli = PHP_SAPI == 'cli' ? '_cli' : '';
+
+        if ($this->config['single']) {
+            $name = is_string($this->config['single']) ? $this->config['single'] : 'single';
+
+            $name .= '_' . $type;
+        } elseif ($this->config['max_files']) {
+            $name = date('Ymd') . '_' . $type . $cli;
+        } else {
+            $name = date('d') . '_' . $type . $cli;
+        }
+
+        return $path . DIRECTORY_SEPARATOR . $name . '.log';
+    }
+
+    /**
+     * 检查日志文件大小并自动生成备份文件
+     * @access protected
+     * @param  string    $destination 日志文件
+     * @return void
+     */
+    protected function checkLogSize($destination)
+    {
+        if (is_file($destination) && floor($this->config['file_size']) <= filesize($destination)) {
+            try {
+                rename($destination, dirname($destination) . DIRECTORY_SEPARATOR . time() . '-' . basename($destination));
+            } catch (\Exception $e) {
+            }
+        }
+    }
+
+    /**
+     * CLI日志解析
+     * @access protected
+     * @param  array     $info 日志信息
+     * @return string
+     */
+    protected function parseCliLog($info)
+    {
+        if ($this->config['json']) {
+            $message = json_encode($info, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "\r\n";
+        } else {
+            $now = $info['timestamp'];
+            unset($info['timestamp']);
+
+            $message = implode("\r\n", $info);
+
+            $message = "[{$now}]" . $message . "\r\n";
+        }
+
+        return $message;
+    }
+
+    /**
+     * 解析日志
+     * @access protected
+     * @param  array     $info 日志信息
+     * @return string
+     */
+    protected function parseLog($info)
+    {
+        $requestInfo = [];
+
+        if ($this->config['json']) {
+            $info = $requestInfo + $info;
+            return json_encode($info, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) . "\r\n";
+        }
+
+        array_unshift($info, "---------------------------------------------------------------\r\n[{$info['timestamp']}] ");
+        unset($info['timestamp']);
+
+        return implode("\r\n", $info) . "\r\n";
+    }
+
+}
